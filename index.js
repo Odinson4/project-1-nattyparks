f880acccd0e892b9345ef6ec80d58a10ed831682
Dylan Abbott <dylanabbott33@yahoo.com>
2023-02-14 13:53:08 -0700

    5th push

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
 const stateSelect = document.querySelector("select");
 const parksList = document.querySelector("#parks-list");
-const parkSection = document.querySelector("#park-section");
 
 const dropdownButton = document.querySelector(".dropbtn");
 dropdownButton.addEventListener("click", () => {
@@ -16,81 +14,52 @@ parksList.addEventListener("click", (event) => {
 stateSelect.addEventListener("change", (event) => {
   const selectedState = event.target.value;
   parksList.innerHTML = "";
-  parkSection.innerHTML = "";
 
-  fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${selectedState}&api_key=feZhEue5BDxWEdkf9Uajlf4ervRQKQWzOswrWVoa&limit=500`)
-    .then((response) => response.json())
-    .then((data) => {
-      const parks = data.data;
+  renderParks(selectedState)
+    .then((parks) => {
       parks.forEach((park) => {
-        const parkItem = document.createElement("card");
-        const address = park.addresses[0];
-        const entranceFees = park.entranceFees[0];
-        const [activity1, activity2, activity3, activity4] = park.activities;
-
-        parkItem.innerHTML = `
-          <img src="${park.images[0].url}"<br><br>
-          <a href="#">${park.fullName}</a>
-          <br>Address: ${address.line1} ${address.city}, ${address.stateCode} ${address.postalCode}
-          <br>Entrance Fees: $${entranceFees.cost}
-          <br>Activities: ${activity1?.name}, ${activity2?.name}, ${activity3?.name}, ${activity4?.name}
-          <br>${park.description}
-        `;
+        const parkItem = createParkCard(park);
         parksList.appendChild(parkItem);
+        const card = parkItem.querySelector('.card');
+        const cardDescription = parkItem.querySelector('.card-text');
+        addCardEventListeners(card, cardDescription);
       });
     });
 });
 
+function renderParks(stateCode) {
+  return fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&api_key=feZhEue5BDxWEdkf9Uajlf4ervRQKQWzOswrWVoa&limit=500`)
+    .then((response) => response.json())
+    .then((data) => data.data);
+}
 
-// let stateUrl = `https://developer.nps.gov/api/v1/parks?parkCode=&api_key=feZhEue5BDxWEdkf9Uajlf4ervRQKQWzOswrWVoa&limit=500`
-// const stateSelect = document.querySelector('select');
-// const parksList = document.querySelector('#parks-list');
-// const parkSection = document.querySelector('#park-section');
-// let park;
+function createParkCard(park) {
+  const address = park.addresses[0];
+  const entranceFees = park.entranceFees[0];
 
-// function handleDropdownClick() {
-//   const dropdownContent = document.querySelector('#state');
-//   dropdownContent.classList.toggle('show');
-// }
-// const dropdownButton = document.querySelector('.dropbtn');
-// dropdownButton.addEventListener('click', handleDropdownClick);
+  const parkItem = document.createElement("card");
+  parkItem.innerHTML = `<div class="card">
+    <img src="${park.images[0].url}">
+    <h2>${park.fullName}</h2>
+    <p class="card-text hide">
+      <a href="${park.url}" class="website">Website</a>
+      <br>
+      Address: ${address.line1} ${address.city}, ${address.stateCode} ${address.postalCode}
+      <br>
+      Entrance Fees: $${entranceFees?.cost}
+      <br>
+      Description: ${park.description}
+    </p>
+  </div>`;
 
-// parksList.addEventListener('click', (event) => {
-//   const parkId = event.target.dataset.parkId;
-//   if (!parkId) {
-//     return;
-//   }
+  return parkItem;
+}
 
-// })
-
-// // This function allows the user to select a state from the dropdown.
-// // The change event is triggered when the user selects a new state.
-// stateSelect.addEventListener('change', (event) => {
-//   const selectedState = event.target.value;
-//   // This resets the list of parks instead of adding the parks onto the list.
-//   parksList.innerHTML = '';
-//   parkSection.innerHTML = '';
-//   console.log(selectedState);
-
-//   // A fetch that is grabbing the data for the selected state.
-//   fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${selectedState}&api_key=feZhEue5BDxWEdkf9Uajlf4ervRQKQWzOswrWVoa&limit=500`)
-//     .then(response => response.json())
-//     .then(data => {
-//       const parks = data.data;
-//       parks.forEach(park => {
-//         console.log(park);
-//         const parkItem = document.createElement('li.card');
-//         // It's grabbing the data for the selected state and displays full name of that park from the array (API)
-//         let address = park.addresses[0];
-//         let entranceFees = park.entranceFees[0]
-//         let activity1 = park.activities[0];
-//         let activity2 = park.activities[1];
-//         let activity3 = park.activities[2];
-//         let activity4 = park.activities[3];
-//         parkItem.innerHTML = `<img src="${park.images[0].url}"<br> <a href="#">${park.fullName}</a> <br>Address: ${address.line1} ${address.city}, ${address.stateCode} ${address.postalCode}<br> Entrance Fees: $${entranceFees.cost}<br>Activities: ${activity1?.name}, ${activity2?.name}, ${activity3?.name}, ${activity4?.name} <br>${park.description}`;
-//         // Appending the park item to the parkSection.
-//         parksList.appendChild(parkItem);
-//       });
-//     });
-// });
-
+function addCardEventListeners(card, cardDescription) {
+  card.addEventListener('mouseover', () => {
+    cardDescription.classList.remove('hide');
+  });
+  card.addEventListener('mouseout', () => {
+    cardDescription.classList.add('hide');
+  });
+}
