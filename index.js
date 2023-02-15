3ca8aaa02c6a912f94afe385dbcf4e08ab744136
Dylan Abbott <dylanabbott33@yahoo.com>
2023-02-15 12:02:57 -0700

    7th push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const stateSelect = document.querySelector("select");
 const parksList = document.querySelector("#parks-list");
-
 const dropdownButton = document.querySelector(".dropbtn");
+
 dropdownButton.addEventListener("click", () => {
   document.querySelector("#state").classList.toggle("show");
 });
@@ -20,7 +20,6 @@ stateSelect.addEventListener("change", (event) => {
       parks.forEach((park) => {
         const parkItem = createParkCard(park);
         parksList.appendChild(parkItem);
-        addCardEventListeners(parkItem); // Add event listeners to the park item
       });
     });
 });
@@ -35,32 +34,56 @@ function createParkCard(park) {
   const address = park.addresses[0];
   const entranceFees = park.entranceFees[0];
 
-  const parkItem = document.createElement("card");
-  parkItem.innerHTML = `<div class="card">
-    <img src="${park.images[0].url}">
+  const parkItem = document.createElement("div");
+  parkItem.classList.add("card");
+  parkItem.innerHTML = `<div class="flip-card">
+    <div class="flip-card-inner">
+    <div class="flip-card-front">
+    <div class="carousel-container">
+    <img id="park-image-${park.id}" src="${park.images[0].url}">
+    </div>
     <h2>${park.fullName}</h2>
-    <p class="card-text hide">
-      <a href="${park.url}" class="website">Website</a>
-      <br>
-      Address: ${address.line1} ${address.city}, ${address.stateCode} ${address.postalCode}
-      <br>
-      Entrance Fees: $${entranceFees?.cost}
-      <br>
-      Description: ${park.description}
+    </div>
+    <div class="flip-card-back">
+    <p class="card-text">
+    Address: ${address.line1} ${address.city}, ${address.stateCode} ${address.postalCode}
+    <br>
+    Entrance Fees: $${entranceFees?.cost}
+    <br>
     </p>
+    <a href="${park.url}" class="website">WEBSITE</a>
+    </div>
+    </div>
   </div>`;
+
+  // Set up the carousel of images
+  const imageElement = parkItem.querySelector(`#park-image-${park.id}`);
+  let imageIndex = 0;
+  setInterval(() => {
+    imageIndex = (imageIndex + 1) % park.images.length;
+    imageElement.src = park.images[imageIndex].url;
+  }, 3000);
 
   return parkItem;
 }
 
-function addCardEventListeners(parkItem) {
-  const card = parkItem.querySelector('.card');
-  const cardDescription = parkItem.querySelector('.card-text');
+const randomParkButton = document.getElementById("random-park-button");
 
-  card.addEventListener('mouseover', () => {
-    cardDescription.classList.remove('hide');
-  });
-  card.addEventListener('mouseout', () => {
-    cardDescription.classList.add('hide');
-  });
+// Add an event listener to the button element
+randomParkButton.addEventListener("click", getRandomPark);
+
+// Define the getRandomPark function
+function getRandomPark() {
+  fetch("https://developer.nps.gov/api/v1/parks?api_key=feZhEue5BDxWEdkf9Uajlf4ervRQKQWzOswrWVoa&limit=500")
+    .then((response) => response.json())
+    .then((data) => {
+      const randomIndex = Math.floor(Math.random() * data.data.length);
+      const park = data.data[randomIndex];
+      const parkCard = createParkCard(park);
+      parksList.innerHTML = "";
+      parksList.appendChild(parkCard);
+    })
+    .catch((error) => {
+      console.error("Error fetching parks:", error);
+    });
 }
